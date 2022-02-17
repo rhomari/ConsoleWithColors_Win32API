08c3192b1bc44c58c7be20ebfe047d38a1a6eced
TARIQ RHOMARI <tariqrhomari@gmail.com>
2022-02-17 22:22:37 +0100

    mixings colors*

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <windows.h>
 #include <stdio.h>
 
-
+#define FOREGROUND_YELLOW  FOREGROUND_GREEN | FOREGROUND_RED
+#define FOREGROUND_PURPLE  FOREGROUND_RED | FOREGROUND_BLUE
+#define FOREGROUND_WHITE  FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN
+#define FOREGROUND_CYAN FOREGROUND_BLUE |  FOREGROUND_GREEN 
 int main(){
     
     HANDLE stdouthandle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -9,7 +12,7 @@ int main(){
     DWORD attributesbackup;
     GetConsoleScreenBufferInfo(stdouthandle, &cshi);
     attributesbackup = cshi.wAttributes;
-    SetConsoleTextAttribute(stdouthandle, FOREGROUND_GREEN );
+    SetConsoleTextAttribute(stdouthandle, FOREGROUND_BLUE);
     printf("A bit of text to test things up.\n");
     SetConsoleTextAttribute(stdouthandle, attributesbackup);
     printf("Back to the old ways.");   
